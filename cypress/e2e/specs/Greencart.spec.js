d978213dc02c13947a3f26f3dae97c9241a9f827
arawat1289 <arawat@fonteva.com>
2023-09-02 15:17:35 -0400

    added 1st scenario and updated readme file

--- cypress/e2e/specs/Greencart.spec.js
+++ cypress/e2e/specs/Greencart.spec.js
@@ -1,13 +1,19 @@
-import GreenCartPage from "../pages/GreencartPage"
+import GreenCartPage from "../pages/GreencartPage";
 const greenCartPage = new GreenCartPage();
 
-beforeEach(() => {
-    cy.visit('seleniumPractise/#/')
-  })
-  describe('Green cart scenarios', () => {
-      it('user is able to search an item', () => {
-        greenCartPage.clickSearchItem();
-      })
-    })
+let data;
+before(() => {
+  cy.fixture("Greencart.json").then((value) => {
+    return data = value
+  });
+});
 
-  
+beforeEach(() => {
+  cy.visit("seleniumPractise/#/");
+});
+
+describe("Green cart scenarios", () => {
+  it("user is able to search an item", () => {
+    greenCartPage.clickSearchItem(data.itemName1);
+  });
+});
