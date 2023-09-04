ba62778b2820625e4139f9c26a7da27cb675dce2
arawat1289 <arawat@fonteva.com>
2023-09-04 13:06:44 -0400

    cypress dashboard configuration

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
   viewportHeight: 1080,
   defaultCommandTimeout: 150000,
   numTestsKeptInMemory: 0,
+  projectId: "nyxe43",
   e2e: {
     baseUrl: "https://rahulshettyacademy.com/",
     supportFile: "cypress/support/e2e.js",
